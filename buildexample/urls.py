1e1670ba9ee98e77faf34121b384164bd04daede
Aidan Liu <boomcreeper296@gmail.com>
2025-10-11 11:54:38 -0400

    Change app name

--- buildexample/urls.py
+++ buildexample/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 
 # SHERRIFF: added the '' path to handle the root url
 urlpatterns = [
-    path('', include('myapp.urls')),
-    path('myapp/', include('myapp.urls')),
+    path('', include('rideshareapp.urls')),
+    path('rideshareapp/', include('rideshareapp.urls')),
     path('admin/', admin.site.urls),
 ]
