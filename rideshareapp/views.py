0a13f7fbf2e485596a84077c5c704a74b0fff49d
Aidan Liu <boomcreeper296@gmail.com>
2025-10-19 09:48:26 -0400

    Add moderator login button and update moderator page
    In reality, the two buttons are the same. They just redirect to different places.

--- rideshareapp/views.py
+++ rideshareapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
+
 
 # SHERRIFF: very basic index page created
 def index(request):
@@ -15,6 +16,6 @@ def moderator(request):
     if not request.user.is_authenticated:
         return redirect('/accounts/login')
     if request.user.groups.filter(name='Moderator').exists():
-        return HttpResponse("Moderator")
+        return render(request, 'rideshareapp/moderator.html')
     else:
-        return HttpResponse("You don't have permission!")
+        return HttpResponseForbidden("You don't have permission!")
