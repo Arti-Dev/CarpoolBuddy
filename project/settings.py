9a250bcfb394cef4ddb6a868bb15881e6785dc6f
Mikejr007 <michealbelachew06@gmail.com>
2025-10-31 04:20:07 -0400

    Implemented AWS S3

--- project/settings.py
+++ project/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'rideshareapp',
+    'storages',
 
     #used chat to fix this line on 10/15/25
     #needed to specify accountsconfig so that signal to create user object was sent on login
@@ -153,11 +154,32 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # SHERRIFF: Added the static_root variable here to fix an erorr with static files not being found
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "us-east-2")
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+AWS_S3_CUSTOM_DOMAIN = os.environ.get(
+    "AWS_S3_CUSTOM_DOMAIN",
+    f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+)
+
+STORAGES = {
+    "default": {        
+        "BACKEND": "project.storage_backend.MediaStorage",
+    },
+    "staticfiles": {    
+        "BACKEND": "project.storage_backend.StaticStorage",
+    },
+}
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
 
 # Default primary key field type
