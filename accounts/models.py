b34f76cbf6f5b204bb0f3cca6abe209eaad8b275
Aidan Liu <boomcreeper296@gmail.com>
2025-11-02 13:43:49 -0500

    Add computing id to profiles

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
+    computing_id = models.CharField(max_length=10, unique=True, null=True, blank=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
