da296d0f90a31b436a8eb463d1100433d5e5f2d3
gmlotfi <genavivelotfi@yahoo.com>
2025-11-01 20:46:10 -0400

    debugged post display

--- rideshareapp/views.py
+++ rideshareapp/views.py
@@ -11,7 +11,7 @@ def index(request):
         posts = Post.objects.all().order_by('-created_at')
         #asked chat for this line on 10/15/25
         #prompt how do i change this to not redirect to accounts/login
-        return render(request, "rideshareapp/home.html", {"user": request.user, "post":posts})
+        return render(request, "rideshareapp/home.html", {"user": request.user, "posts":posts})
     else:
         return redirect('/accounts/login')
 
