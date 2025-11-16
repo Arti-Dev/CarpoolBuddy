c75ba443f9dde68d9932cc3d8885429bacae91cc
areebKhan21 <areeb.khan3000@gmail.com>
2025-11-16 15:23:07 -0500

    Updated urls.py with necessary paths

--- rideshareapp/urls.py
+++ rideshareapp/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('moderator/', views.moderator, name='moderator'),
-    path("admin-dashboard/", views.admin_dashboard, name="admin-dashboard")
+    path("admin-dashboard/", views.admin_dashboard, name="admin-dashboard"),
+    path('admin/add-user/', views.admin_add_user, name='admin-add-user'),
+    path('admin/create-event/', views.admin_create_event, name='admin-create-event'),
+    path('admin/my-groups/', views.admin_my_groups, name='admin-my-groups')
 ]
