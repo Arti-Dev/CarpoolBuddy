ddac2cf697a648540f942c79efd38140f037c4d9
Aidan Liu <boomcreeper296@gmail.com>
2025-11-20 16:55:52 -0500

    Computing ID validation

--- chat/urls.py
+++ chat/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('start_chat/', views.start_chat, name='start-chat'),
     path('start_group_chat/', views.start_group_chat, name='start-group-chat'),
     path("<int:room_id>/", views.room_view, name="room"),
+    path("validate_other_user/", views.validate_other_user, name="validate-other-user"),
 ]
