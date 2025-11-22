d736447818edfb2770ed7ad18ac4427bc0973068
areebKhan21 <areeb.khan3000@gmail.com>
2025-11-22 05:51:50 -0500

    updated urls.py for admin cio merge

--- rideshareapp/urls.py
+++ rideshareapp/urls.py
@@ -1,13 +1,12 @@
-from django.urls import path, include
-
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('moderator/', views.moderator, name='moderator'),
-    path("admin-dashboard/", views.admin_dashboard, name="admin-dashboard"),
+    path('admin-dashboard/', views.admin_dashboard, name='admin-dashboard'),
     path('admin/add-user/', views.admin_add_user, name='admin-add-user'),
     path('admin/create-cio/', views.admin_create_cio, name='admin-create-cio'),
-    path('admin/my-groups/', views.admin_my_groups, name='admin-my-groups')
+    path('admin/my-groups/', views.admin_my_groups, name='admin-my-groups'),
     path('cio-dashboard/', views.cio_dashboard, name='cio_dashboard'),
 ]
