6a58086f0fbdab72051066a3d5ff3956e308ab9e
Aidan Liu <boomcreeper296@gmail.com>
2025-12-09 00:48:56 -0500

    All profiles are viewable now

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('profile/', views.profile, name='profile'),
-    path("profile/edit/", views.profile_edit, name="profile_edit"),
+    path('profile/', views.profile_self, name='profile'),
+    path('profile/<int:user_id>', views.profile, name='profile'),
+    path("profileedit/", views.profile_edit, name="profile_edit"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import redirect
 from .forms import UserUpdateForm, ProfileUpdateForm
 from posts.models import Post
 
+
 @login_required
-def profile(request):
+def profile(request, user_id):
+    if user_id is None: # Viewing own profile
+        profile_user = request.user
+    else:
+        profile_user = get_object_or_404(User, pk=user_id)
+
+    # Get profile object
+
+
     #add posts so that a users posts can be seen on profile page
-    posts = Post.objects.filter(author=request.user).order_by('-created_at')
-    return render(request, 'account/profile.html', {"posts":posts})
+    posts = Post.objects.filter(author=profile_user).order_by('-created_at')
+    return render(request, 'account/profile.html', {"profile_user":profile_user, "posts":posts})
+
+@login_required
+def profile_self(request):
+    return profile(request, request.user.id)
+
 @login_required
 def profile_edit(request):
     if request.method == "POST":
@@ -23,4 +38,4 @@ def profile_edit(request):
     else:
         user_update_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
-    return render(request, "account/profile_edit.html", {"user_update_form": user_update_form, "profile_form": profile_form})
+    return render(request, "account/profileedit.html", {"user_update_form": user_update_form, "profile_form": profile_form})
