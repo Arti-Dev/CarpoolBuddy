ddd86423b130476b7d611b997a8fbeb3070b4b73
madsej <147103124+madsej@users.noreply.github.com>
2025-12-09 20:08:42 -0500

    Finish adding sign up button

--- posts/models.py
+++ posts/models.py
@@ -13,6 +13,8 @@ class Post(models.Model):
     end_location = models.CharField(max_length=100)
     departure_time = models.DateTimeField()
     num_riders = models.PositiveIntegerField(default=1)
+    passengers = models.ManyToManyField(User, related_name='joined_rides', blank=True)
+
     photo = models.ImageField(upload_to="post_photos/", null=True, blank=True)
 
     photo_visibility = models.CharField(
@@ -44,3 +46,14 @@ class Post(models.Model):
             return True
         return False
 
+    @property
+    def current_passengers_count(self):
+        return self.passengers.count()
+
+    @property
+    def seats_left(self):
+        return max(0, self.num_riders - self.current_passengers_count)
+
+    @property
+    def is_full(self):
+        return self.seats_left <= 0
